9786382710783666e86ee990daaeaf6f1b9148a0
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-07-04 23:39:03 -0400

    adding more tasks and pretty printing of die objects

--- bohnanza.py
+++ bohnanza.py
@@ -6,8 +6,27 @@ class Die(object):
     def __init__(self, faces):
         self.faces = faces
 
+    def __str__(self):
+        return str(self.faces)
+
+    def __repr__(self):
+        return self.__str__()
+
     def roll(self):
         return self.faces[randint(0, len(self.faces) - 1)]
+
+class MonoDie(Die):
+    def __init__(self, face):
+        self.face = face
+
+    def __str__(self):
+        return str(self.face)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def roll(self):
+        return self.face
 
 class WhiteDie(Die):
     def __init__(self):
@@ -61,7 +80,8 @@ class ExceptTask(Task):
         return 'any %d dice except %s' % (self.total, ' or '.join(self.colors))
 
     def check(self, faces):
-        return len(faces) >= self.total and not any((face == color for face in faces for color in self.colors))
+        return (len(faces) >= self.total
+                and not any((face == color for face in faces for color in self.colors)))
 
 class TuplesTask(Task):
     def __init__(self, size, tuples):
@@ -108,12 +128,12 @@ class FullHouseTask(Task):
 
     def __str__(self):
         if self.total == 5 and self.count1 == 3:
-            return ('full house, one color %s, the other %s' 
+            return ('full house, one color %s, the other %s'
                         % (self.color1, 'free' if self.color2 is None else self.color2))
         else:
-            return ('full house with %d dice, one tuple %d, the other %d, one color %s, the other %s' 
-                        % (self.total, self.count1, self.count2, self.color1, 
-                        'free' if self.color2 is None else self.color2))
+            return ('full house with %d dice, one tuple %d, the other %d, one color %s, the other %s'
+                        % (self.total, self.count1, self.count2, self.color1,
+                           'free' if self.color2 is None else self.color2))
 
     def check(self, faces):
         c = Counter(faces)
@@ -155,6 +175,20 @@ if __name__ == '__main__':
             ColorCountTask(6),
             ColorCountTask(7)
         ]
+    extra_tasks = [
+        CombinationTask({'blue': 2, 'black': 1}),
+        CountTask(3, ['orange', 'green']),
+        CombinationTask({'green': 1, 'black': 2})
+    ]
+    tasks += extra_tasks
+    tasks += [AllTask(extra_tasks[:2]), AllTask(extra_tasks)]
 
+    print(dice)
+    for task in tasks:
+        print('%s: %.1f%%' % (task, task.probability(dice, n=n) * 100))
+
+    dice = dice[1:] + [MonoDie('black')]
+
+    print(dice)
     for task in tasks:
         print('%s: %.1f%%' % (task, task.probability(dice, n=n) * 100))
